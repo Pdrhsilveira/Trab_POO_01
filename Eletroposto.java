b3e14fab53ad3a0a152d70ba81aa6bfe6fbf2e91
Pedro Henrique Nunes Silveira <127452266+Pdrhsilveira@users.noreply.github.com>
2024-11-03 02:03:45 -0300

    Create Eletroposto.java

---
+++ Eletroposto.java
@@ -0,0 +1,39 @@
+class Eletroposto {
+    private int id;
+    private String local;
+    private int numVagas;
+    private int vagasOcupadas;
+    private int tempoCarga;
+
+    public Eletroposto(int id, String local, int numVagas, int tempoCarga) {
+        this.id = id;
+        this.local = local;
+        this.numVagas = numVagas;
+        this.vagasOcupadas = 0;
+        this.tempoCarga = tempoCarga;
+    }
+
+    public boolean verificarDisponibilidade() {
+        return vagasOcupadas < numVagas;
+    }
+
+    public void ocuparVaga() {
+        if (verificarDisponibilidade()) {
+            vagasOcupadas++;
+            System.out.println("Vaga ocupada no Eletroposto " + id);
+        } else {
+            System.out.println("Eletroposto " + id + " está cheio.");
+        }
+    }
+
+    public void liberarVaga() {
+        if (vagasOcupadas > 0) {
+            vagasOcupadas--;
+            System.out.println("Vaga liberada no Eletroposto " + id);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+}
